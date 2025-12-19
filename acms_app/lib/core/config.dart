0502fd70e310a33ff8b7918c296db0d462313a32
rajveergoudxd <goudrajveer8@gmail.com>
2025-12-20 00:23:20 +0530

    update

--- acms_app/lib/core/config.dart
+++ acms_app/lib/core/config.dart
@@ -1,5 +1,6 @@
 class Config {
   // Use 10.0.2.2 for Android Emulator, or your local IP for physical devices
-  static const String baseUrl = 'http://192.168.1.10:8000/api/v1';
+  static const String baseUrl =
+      'https://vextra-backend-842753730816.us-central1.run.app/api/v1';
   static const String appName = 'Vextra';
 }
